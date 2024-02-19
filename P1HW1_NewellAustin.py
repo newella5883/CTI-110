d0cc423462fe60483bcfed2a5b33aab7f2f481ec
newella5883 <160151221+newella5883@users.noreply.github.com>
2024-02-19 16:11:47 -0500

    Add files via upload
    First HW Submission

---
+++ P1HW1_NewellAustin.py
@@ -0,0 +1,22 @@
+# Austin Newell
+
+# 2-19-2024
+
+# P1HW1
+
+# How to write code that collects information
+
+user_int = int(input('Enter integer:\n'))
+
+print('You Entered:', user_int)
+
+print(user_int, "Squared is", user_int ** 2)
+
+print('And', user_int, 'Cubed is', user_int ** 3, '!!')
+
+user_int2 = int(input('Enter another integer:\n'))
+
+print('4 +', user_int2, 'is', user_int2 + 4)
+
+print('4 *', user_int2, 'is', user_int2 * 4)
+
