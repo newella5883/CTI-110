5a887e5c0dd370ec6243071bc7c9984617f1228f
newella5883 <160151221+newella5883@users.noreply.github.com>
2024-02-20 16:00:23 +0000

    HW_Example

--- HW_Example2.py
+++ HW_Example2.py
@@ -8,4 +8,14 @@ street = input('What is your street?')
 #Convert to int() in the same line
 house_num = int(input('What is your house number?'))
 
-house_num = input('What is your house number?')
+
+#Convert to int() separatly
+house_num = int(house_num)
+
+#Calculate neighbor's house number
+neigh_num = house_num + 2
+
+#Print our address and neighbor address
+print('Your address is', house_num, street)
+
+print('Your neighbor address is', neigh_num, street)
